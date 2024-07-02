39bc8ccfc81cb9094da6206b70b90b2b5ce06590
PoopFather <111574562+PoopFather@users.noreply.github.com>
2024-07-02 14:42:08 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+ 
+setup(
+      name='face2face',
+      packages=find_packages(),
+      version='0.0.1', 
+      description='Fit 3D morphable model to face & track them',
+      author='Karim Shaban',
+)
